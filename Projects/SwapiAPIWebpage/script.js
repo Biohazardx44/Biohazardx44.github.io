a8a36d83aaff91e1ae4a198f6efa43d50f109b72
Biohazardx44 <nikolailievski420@gmail.com>
2023-02-28 23:54:31 +0100

    Swapi API Webpage Version 1.0.0

--- Projects/SwapiAPIWebpage/script.js
+++ Projects/SwapiAPIWebpage/script.js
@@ -1,24 +1,50 @@
-const button = document.getElementsByTagName('button');
+/**
+ * Swapi API Webpage
+ * Created by Nikola Ilievski
+ * Version: 1.0.0
+ */
+
+const button = document.getElementsByTagName('input');
 const mainDiv = document.getElementById('mainDiv');
 const container = document.querySelector('.buttons-container');
 
+/**
+ * A function that fetches the data from the Swapi API
+ * @param {object} apiUrl and stores the response and
+ * @returns the data
+ */
 async function fetchData(apiUrl) {
   const response = await fetch(apiUrl);
   const data = await response.json();
   return data;
 };
 
+/**
+ * A function that generates buttons for a given
+ * @param {string} type
+ */
 async function generateButtons(type) {
   const apiUrl = `https://swapi.dev/api/${type}/`;
 
-  let data = sessionStorage.getItem(type);
+  /**
+   * A function that is used to get data from storage. It takes
+   * @param {object} apiUrl 
+   * @param {string} storageKey 
+   * @returns the data
+   */
+  async function getDataFromStorage(apiUrl, storageKey) {
+    let data = sessionStorage.getItem(storageKey);
 
-  if (!data) {
-    data = await fetchData(apiUrl);
-    sessionStorage.setItem(type, JSON.stringify(data));
-  } else {
-    data = JSON.parse(data);
+    if (!data) {
+      data = await fetchData(apiUrl);
+      sessionStorage.setItem(storageKey, JSON.stringify(data));
+    } else {
+      data = JSON.parse(data);
+    };
+    return data;
   };
+
+  data = await getDataFromStorage(apiUrl, type);
 
   const buttonsDiv = document.createElement('div');
   buttonsDiv.classList.add('buttons-container');
@@ -37,14 +63,8 @@ async function generateButtons(type) {
       const apiUrl = `https://swapi.dev/api/${type}/?search=${encodeURIComponent(name)}`;
 
       mainDiv.innerHTML = '';
-      let data = sessionStorage.getItem(apiUrl);
 
-      if (!data) {
-        data = await fetchData(apiUrl);
-        sessionStorage.setItem(apiUrl, JSON.stringify(data));
-      } else {
-        data = JSON.parse(data);
-      };
+      data = await getDataFromStorage(apiUrl, apiUrl);
 
       const itemData = data.results[0];
       const itemDiv = document.createElement('div');
@@ -100,6 +120,11 @@ boxes.forEach((box) => {
   });
 });
 
+/**
+ * A function that creates a button element with the given
+ * @param {string} text as its innerText
+ * @returns the created button element
+ */
 function createButton(text) {
   const button = document.createElement('button');
   button.innerText = text;
@@ -108,6 +133,8 @@ function createButton(text) {
   button.style.right = '25px';
   button.style.fontSize = '2rem';
   button.style.padding = '10px 20px';
+  button.style.border = '2px solid var(--primary-yellow)';
+  button.style.cursor = 'pointer';
   return button;
 };
 
