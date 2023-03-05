56b3198591a11c4b5adcc9e8380c87c0cff5e5b2
Biohazardx44 <nikolailievski420@gmail.com>
2023-03-05 21:31:04 +0100

    Weather App Version 1.0.1 Minor Bugs Fixed

--- Projects/WeatherApp/src/script.js
+++ Projects/WeatherApp/src/script.js
@@ -5,7 +5,7 @@ import CardTemplates from "./helperFunctions/cardTemplates.js";
 /**
  * Weather App
  * Created by Nikola Ilievski
- * Version: 1.0.0
+ * Version: 1.0.1
  */
 
 const apiParameters = {
